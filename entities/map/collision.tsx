d84602873a1a2fa11b7b9e34afd5d1779ce91ec9
fguan0529 <guan.fanny@gmail.com>
2017-11-20 05:42:01 -0800

    Manual map rendering testing

---
+++ entities/map/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="collision" tilewidth="192" tileheight="192" tilecount="100" columns="10">
+ <image source="collision.png" trans="fd9ba8" width="2000" height="2000"/>
+</tileset>
